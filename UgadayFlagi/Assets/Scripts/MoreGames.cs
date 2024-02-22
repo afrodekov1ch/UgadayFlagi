dd240143f7f43eae56c7f7807f5247e05a20cf13
afrodekov1ch <omurcomp@gmail.com>
2024-02-22 19:14:40 +0600

    аааах
    сэкс

---
+++ UgadayFlagi/Assets/Scripts/MoreGames.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreGames : MonoBehaviour
+{
+    public void https()
+    {
+        Application.OpenURL("https://yandex.ru/games/developer?name=Akaha%20Group#redir-data=%7B%22http_ref%22%3A%22https%253A%252F%252Fyandex.ru%252Fgames%252F%2523app%253D290589%22%2C%22rn%22%3A414584858%7D");
+    }
+}
